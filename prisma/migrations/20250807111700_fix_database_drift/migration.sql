a47b0d399e8df2676740e9955229a2f1bbe9425b
star7th <xing7th@gmail.com>
2025-08-07 02:29:35 +0800

    update

--- prisma/migrations/20250807111700_fix_database_drift/migration.sql
+++
@@ -1,5 +0,0 @@
--- 添加 displayOrder 字段到 Monitor 表
-ALTER TABLE "Monitor" ADD COLUMN "displayOrder" INTEGER;
-
--- 添加 defaultForNewMonitors 字段到 NotificationChannel 表
-ALTER TABLE "NotificationChannel" ADD COLUMN "defaultForNewMonitors" BOOLEAN DEFAULT false; 
